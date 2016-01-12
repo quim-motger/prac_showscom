2079c7fa15239d4636149edfd052ee5a7f361a02
jmotger <joaquim.motger@est.fib.upc.edu>
2016-01-12 19:31:45 +0100

    comprarentrada controller presentation

---
+++ src/main/java/edu/upc/as/presentation/ComprarEntradaController.java
@@ -0,0 +1,35 @@
+package edu.upc.as.presentation;
+
+import edu.upc.as.domain.utils.InfoOcupacio;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by jmotger on 12/01/16.
+ */
+public class ComprarEntradaController {
+
+    private int nEspec;
+
+    public void prOkConsultaRepresentacions(String titol, Date data) {
+
+    }
+
+    public void prOkConsultaOcupacions(String nomLocal, String sessio, int nombEspectador) {
+
+    }
+
+    public void prOkSeleccionaSeients(List<InfoOcupacio> seients) {
+
+    }
+
+    public void prObtePreuMoneda(String moneda) {
+
+    }
+
+    public void prOkPagament(String dni, int codiB, String numCompte) {
+
+    }
+
+}
