e3252e18da8373d2a4998a400ad3c2a511b2c996
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-01-12 21:48:53 +0100

    speedment commit

--- tables.sql
+++ tables.sql
@@ -1,62 +1,64 @@
 CREATE TABLE entrada
 (
-  dniclient VARCHAR(30) NOT NULL,
-  nombreespectadors INT NOT NULL,
-  data DATE NOT NULL,
-  preu REAL NOT NULL,
-  sessio VARCHAR(20) NOT NULL,
-  nomlocal VARCHAR(255) NOT NULL,
-  identificador SERIAL PRIMARY KEY NOT NULL
+  dniclient VARCHAR(30),
+  nombreespectadors INT,
+  data DATE ,
+  preu REAL ,
+  sessio VARCHAR(20) ,
+  nomlocal VARCHAR(255),
+  identificador SERIAL PRIMARY KEY
 );
 CREATE TABLE espectacle
 (
-  titol VARCHAR(255) PRIMARY KEY NOT NULL,
-  participants INT DEFAULT 0 NOT NULL
+  titol VARCHAR(255) PRIMARY KEY ,
+  participants INT DEFAULT 0 
 );
 CREATE TABLE estrena
 (
-  nomlocal VARCHAR(255) NOT NULL,
-  sessio VARCHAR(20) NOT NULL,
-  recarrec INT NOT NULL,
+  nomlocal VARCHAR(255),
+  sessio VARCHAR(20),
+  recarrec INT ,
   PRIMARY KEY (nomlocal, sessio)
 );
 CREATE TABLE local
 (
-  nom VARCHAR(255) PRIMARY KEY NOT NULL,
-  adreça VARCHAR(255) NOT NULL
+  nom VARCHAR(255) PRIMARY KEY ,
+  adreça VARCHAR(255) 
 );
 CREATE TABLE representacio
 (
-  titolespectacle VARCHAR(255) NOT NULL,
-  sessio VARCHAR(20) NOT NULL,
-  nomlocal VARCHAR(255) NOT NULL,
-  preu REAL NOT NULL,
-  data DATE NOT NULL,
-  nombreseienslliures INT NOT NULL,
+  titolespectacle VARCHAR(255) ,
+  sessio VARCHAR(20) ,
+  nomlocal VARCHAR(255) ,
+  preu REAL ,
+  data DATE ,
+  nombreseienslliures INT ,
   PRIMARY KEY (sessio, nomlocal)
 );
 CREATE TABLE seient
 (
-  fila INT NOT NULL,
-  columna INT NOT NULL,
-  nomlocal VARCHAR(255) NOT NULL,
+  fila INT ,
+  columna INT ,
+  nomlocal VARCHAR(255) ,
   PRIMARY KEY (fila, columna, nomlocal)
 );
 CREATE TABLE seient_en_representacio
 (
-  sessio VARCHAR(20) NOT NULL,
-  nomlocalrep VARCHAR(255) NOT NULL,
-  nomlocal VARCHAR(255) NOT NULL,
-  fila INT NOT NULL,
-  columna INT NOT NULL,
-  estat VARCHAR(50) NOT NULL,
-  identificadorentrada INT NOT NULL,
+  sessio VARCHAR(20) ,
+  nomlocalrep VARCHAR(255) ,
+  nomlocal VARCHAR(255) ,
+  fila INT ,
+  columna INT ,
+  estat VARCHAR(50) ,
+  identificadorentrada INT ,
   PRIMARY KEY (sessio, nomlocal, fila, columna)
 );
 CREATE TABLE sessio
 (
-  sessio VARCHAR(20) PRIMARY KEY NOT NULL
+  sessio VARCHAR(20) PRIMARY KEY 
 );
+
+
 ALTER TABLE entrada ADD FOREIGN KEY (sessio, nomlocal) REFERENCES representacio (sessio, nomlocal);
 ALTER TABLE estrena ADD FOREIGN KEY (nomlocal, sessio) REFERENCES representacio (sessio, nomlocal);
 ALTER TABLE representacio ADD FOREIGN KEY (titolespectacle) REFERENCES espectacle (titol);
